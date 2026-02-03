36db8df71aeb7b22459204d20a836ed35eef0e23
Raspi_dude <131214288+raspi-dude@users.noreply.github.com>
2026-02-02 17:03:16 -0800

    Update entrypoint.sh

--- containers/beammp/entrypoint.sh
+++ containers/beammp/entrypoint.sh
@@ -1,3 +1,4 @@
+set -e
 # Default the TZ environment variable to UTC.
 TZ=${TZ:-UTC}
 export TZ
@@ -18,5 +19,5 @@ PARSED=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g' | eval echo "$(cat
 # from the container itself.
 printf "\033[1m\033[33mcontainer@pterodactyl~ \033[0m%s\n" "$PARSED"
 # shellcheck disable=SC2086
-exec env ${PARSED}
+exec sh -c "$PARSED"
 
