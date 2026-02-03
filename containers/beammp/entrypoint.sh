a62cb41482287d12427d1fdab3676888715e6110
Raspi_dude <131214288+raspi-dude@users.noreply.github.com>
2026-02-02 17:43:16 -0800

    Update entrypoint.sh

--- containers/beammp/entrypoint.sh
+++ containers/beammp/entrypoint.sh
@@ -7,6 +7,11 @@ echo "testing wsg guys"
 MODIFIED_STARTUP=$(eval echo "$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g')")
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
-# Run the Server directly (not with exec) to see what happens
-${MODIFIED_STARTUP}
+# Run server in background
+${MODIFIED_STARTUP} &
+SERVER_PID=$!
+
+# Wait for up to 5 seconds after the wait command
+timeout 5s bash -c "wait $SERVER_PID" 2>/dev/null || kill -9 $SERVER_PID 2>/dev/null
+
 echo "BeamMP process ended, script exiting..."
