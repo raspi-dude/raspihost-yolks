15db1f3eb03241ee0e0ddbf706e5250563a03088
Raspi_dude <131214288+raspi-dude@users.noreply.github.com>
2026-02-02 17:41:03 -0800

    Update entrypoint.sh

--- containers/beammp/entrypoint.sh
+++ containers/beammp/entrypoint.sh
@@ -7,5 +7,6 @@ echo "testing wsg guys"
 MODIFIED_STARTUP=$(eval echo "$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g')")
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
-# Run the Server - exec replaces this shell with BeamMP
-exec ${MODIFIED_STARTUP}
+# Run the Server directly (not with exec) to see what happens
+${MODIFIED_STARTUP}
+echo "BeamMP process ended, script exiting..."
