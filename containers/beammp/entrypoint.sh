e106554d2acf02c20044c1e922a2bbd654f07c00
Raspi_dude <131214288+raspi-dude@users.noreply.github.com>
2026-02-03 19:41:18 -0800

    Update entrypoint.sh

--- containers/beammp/entrypoint.sh
+++ containers/beammp/entrypoint.sh
@@ -1,22 +1,15 @@
 #!/bin/bash
 cd /home/container
 
-echo "testing wsg guys"
-
-# Replace Startup Variables
 MODIFIED_STARTUP=$(eval echo "$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g')")
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
-# Run the Server
-${MODIFIED_STARTUP} &
-SERVER_PID=$!
+# Watchdog: After 10 seconds of inactivity post-shutdown, force kill
+(sleep 10; pkill -9 -f "BeamMP-Server") &
+KILLER_PID=$!
 
-# Wait for the server process
-wait $SERVER_PID
+eval ${MODIFIED_STARTUP}
 
-# After BeamMP "exits" (but hangs), give it 5 seconds then force kill
-sleep 5
-kill -9 $SERVER_PID 2>/dev/null
-
-echo "Server exited with code 0"
+# If server exits cleanly, kill the watchdog  
+kill $KILLER_PID 2>/dev/null
 exit 0
