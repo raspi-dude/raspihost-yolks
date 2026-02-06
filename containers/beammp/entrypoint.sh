d070b3891ab38819a8f9ee7847f318d4b9ab2e2a
Raspi_dude <131214288+raspi-dude@users.noreply.github.com>
2026-02-05 22:54:39 -0800

    Update entrypoint.sh

--- containers/beammp/entrypoint.sh
+++ containers/beammp/entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 cd /home/container
 
+# Disable job control messages
+set +m
+
 MODIFIED_STARTUP=$(eval echo "$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g')")
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
@@ -10,11 +13,8 @@ eval ${MODIFIED_STARTUP} 2>&1 | while IFS= read -r line; do
     
     # Look for shutdown message, but NOT if the line contains [CHAT]
     if echo "$line" | grep -q "\[INFO\] Shutdown\." && ! echo "$line" | grep -q "\[CHAT\]"; then
-        (sleep 2; pkill -9 -f "BeamMP-Server" 2>/dev/null) &
+        (sleep 2; pkill -9 -f "BeamMP-Server") &
     fi
 done
-
-# Suppress the "Killed" message
-wait 2>/dev/null
 
 exit 0
