d3563ce94d317ed68e25628f5fddf1e4562105a9
Raspi_dude <131214288+raspi-dude@users.noreply.github.com>
2026-02-05 22:41:25 -0800

    Update entrypoint.sh

--- containers/beammp/entrypoint.sh
+++ containers/beammp/entrypoint.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
-set -e
-
 cd /home/container
 
-echo "testing wsg guys"
-
 MODIFIED_STARTUP=$(eval echo "$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g')")
-
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
-exec ${MODIFIED_STARTUP}
+# Run BeamMP and pipe output through a monitor
+eval ${MODIFIED_STARTUP} 2>&1 | while IFS= read -r line; do
+    echo "$line"
+    
+    # When we see "Shutdown." in the output, kill BeamMP after 2 seconds
+    if echo "$line" | grep -q "Shutdown\."; then
+        (sleep 2; pkill -9 -f "BeamMP-Server") &
+    fi
+done
+
+exit 0
